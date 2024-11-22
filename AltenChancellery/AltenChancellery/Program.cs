55a2795fb94dd5a4f69cb049ce6d46f6af229020
Flavio DELLA CORTE <flavio.dellacorte1997@gmail.com>
2024-11-22 12:21:12 +0100

    Added Db Configuration

--- AltenChancellery/AltenChancellery/Program.cs
+++ AltenChancellery/AltenChancellery/Program.cs
@@ -1,4 +1,5 @@
 using DBLayer.DBContext;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
--- AltenChancellery/DBLayer/DBContext/ApplicationDBContext.cs
+++ AltenChancellery/DBLayer/DBContext/ApplicationDBContext.cs
@@ -9,6 +9,10 @@ namespace DBLayer.DBContext
 {
     public class ApplicationDBContext: DbContext
     {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        { 
+        }
+
 
     }
 }
