477a43e38d42306b3e44ccf6180b2895e0c15fb9
Flavio DELLA CORTE <flavio.dellacorte1997@gmail.com>
2024-11-22 12:00:24 +0100

    push

--- AltenChancellery/AltenChancellery/Program.cs
+++ AltenChancellery/AltenChancellery/Program.cs
@@ -1,4 +1,25 @@
+using DBLayer.DBContext;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//GET Current Enviroment
+var enviroment = builder.Environment;
+
+//Configuration for the enviroment appsettings file
+builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+          .AddJsonFile($"appsettings.{enviroment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
+//GET DB connection string
+var connectionString = builder.Configuration.GetConnectionString("DBConnection");
+
+
+//Add DB Connection
+builder.Services.AddDbContext<ApplicationDBContext>(
+    options =>
+    options.UseSqlServer(connectionString)
+    );
+
 
 // Add services to the container.
 builder.Services.AddRazorPages();
