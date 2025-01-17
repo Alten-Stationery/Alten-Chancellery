e56a3e1299edf16e6f83c31e43dd72561c396a44
Samuele Sicoli <samuele.sicoli@alten.it>
2025-01-17 12:31:33 +0100

    migration lineup

---
+++ AltenChancellery/DBLayer/Migrations/20250117112903_LineupUserIdColumnSpecifics.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DBLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class LineupUserIdColumnSpecifics : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "AspNetUsers",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldDefaultValue: "NEWID()");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "AspNetUsers",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "NEWID()",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
